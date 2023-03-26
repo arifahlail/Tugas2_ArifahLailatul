a262dd37c166f863c310fa9cf57b2187abd90165
arifahlail <127312731+arifahlail@users.noreply.github.com>
2023-03-25 21:36:17 -0700

    Create PrintXWhile.java

---
+++ Tugas2_SourceCode/PrintXWhile.java
@@ -0,0 +1,33 @@
+/*Nama     : Arifah Lailatul Isthi'anah*/
+/*NIM      : 13020210017*/
+/*Hari/Tgl : Jumat, 24 Maret 2023*/
+/*Waktu    : 19.31 PM*/
+
+import java.util.Scanner;
+
+/* contoh baca nilai x, */
+/* Jumlahkan nilai yang dibaca dengan WHILE */
+public class PrintXWhile {
+    
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus : */
+        int Sum;
+        int x;
+        Scanner masukan=new Scanner(System.in);
+        /* Program */
+        Sum = 0;    /* Inisialisasi */
+        System.out.print ("Masukkan nilai x (int), akhiri dg 999 : ");
+        x = masukan.nextInt();      /* First Elmt*/
+        while (x != 999)    /* Kondisi berhenti */ {
+            Sum = Sum + x; /* Proses */
+            System.out.print("Masukkan nilai x (int), akhiri dg 999 : ");
+            x = masukan.nextInt();  /* First Elmt*/
+        }
+        System.out.println("Hasil penjumlahan = "+ Sum);    /*Terminasi */
+        
+    }
+}
