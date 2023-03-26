58b71fce35c3709f5fbbd0398728e61bbee0a9d4
arifahlail <127312731+arifahlail@users.noreply.github.com>
2023-03-25 21:21:53 -0700

    Create ForEver.java

---
+++ Tugas2_SourceCode/ForEver.java
@@ -0,0 +1,22 @@
+/*Nama      : Arifah Lailatul Isthi'anah*/
+/*NIM       : 13020210017*/
+/*Hari/Tgl  : Jumat, 24 Maret 2023*/
+/*Waktu     : 16.59 PM*/
+
+public class ForEver {
+/**
+* @param args
+*/
+
+  public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Program */
+
+  System.out.println("Program akan looping, akhiri dengan ^c");
+while (true)
+{ System.out.print ("Print satu baris...\n");
+        }
+
+    }
+
+}
